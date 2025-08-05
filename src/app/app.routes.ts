dec10b1915b01de14c36fbf65ede9d37414e914b
ValeriaXZ1 <lesvale568@gmail.com>
2025-08-04 20:02:35 -0500

    Cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FormularioComponent } from './componentes/formulario/formulario.component';
 import { ActualizarComponent } from './componentes/actualizar/actualizar.component';
+import { NostrosComponent } from './componentes/nostros/nostros.component';
 
 export const routes: Routes = [
     {
-        path:'' , component:HomeComponent
+        path: '', component: HomeComponent
     },
     {
-        path:'formulario' , component:FormularioComponent
+        path: 'formulario', component: FormularioComponent
     },
     {
-        path:'autos/:id', component: ActualizarComponent
+        path: 'autos/:id', component: ActualizarComponent
+    },
+    {
+        path: 'nosotros', component: NostrosComponent
     }
 ];
